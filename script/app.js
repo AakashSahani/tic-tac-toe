698e710e2ca7eeb9fa040b2ba63982732aeded57
AakashSahani <sahani.aakash120@gmail.com>
2023-03-26 21:08:07 -0400

    refactored local storage add button function on result modal

--- script/app.js
+++ script/app.js
@@ -4,15 +4,10 @@ const playerChoice = document.getElementById('player');
 const gameAgainstCPU = document.getElementById('cpu');
 const gameAgainstPlayer = document.getElementById('secondPlayer');
 
-//HTML5 local storage
+localStorage.setItem('cpu', '');
+localStorage.setItem('secondPlayer', '');
 localStorage.setItem('firstPlayer', 'X');
-// localStorage.setItem('player2', '');
-// localStorage.setItem('cpu', '');
-// localStorage.setItem('wins', '');
-// localStorage.setItem('ties', '');
-// localStorage.setItem('loss', '');
-
-let firstPlayer = localStorage.firstPlayer;
+let firstPlayer = 'X';
 
 playerChoice.addEventListener('input', (e) => {
 	const isChecked = e.target.checked;
@@ -22,7 +17,6 @@ playerChoice.addEventListener('input', (e) => {
 		localStorage.setItem('firstPlayer', 'O');
 	}
 	firstPlayer = localStorage.firstPlayer;
-	console.log('Player1 ', firstPlayer);
 });
 
 gameAgainstCPU.addEventListener('click', () => {
--- script/game.js
+++ script/game.js
@@ -1,45 +1,73 @@
+// Getting Game Grid Element
 const gameGrid = document.getElementById('gameGrid');
 
+// Tile Options
 const xTileSVG = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z" fill="#31C3BD" fill-rule="evenodd"/></svg>`;
 const oTileSVG = `<svg width="64" height="64" xmlns="http://www.w3.org/2000/svg"><path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z" fill="#F2B137"/></svg>`;
 
+// Result Modal
 const resultModal = document.getElementById('result');
+const result_card = document.getElementById('result_card');
 
+// Result State
 const wins = document.getElementById('wins');
 const ties = document.getElementById('ties');
 const loss = document.getElementById('loss');
 
-const result_card = document.getElementById('result_card');
+localStorage.wins = '0';
+localStorage.ties = '0';
+localStorage.loss = '0';
 
-let turn = 'X';
-
-// Receiving the data:
-localStorage.getItem('variableName');
-
-let player1 = localStorage.getItem('player1');
-
-let player2 = localStorage.player2;
-let cpu = localStorage.cpu;
 let winNum = parseInt(localStorage.wins);
 let tiesNum = parseInt(localStorage.ties);
 let lossNum = parseInt(localStorage.loss);
 
+// Get player choices
+let player1 = localStorage.getItem('firstPlayer') || 'X';
+
+// Reset player2 if cpu is chosen
+localStorage.cpu != '' && localStorage.setItem('secondPlayer', '');
+let player2 = localStorage.secondPlayer;
+
+// Reset cpu if player2 is chosen
+player2 != '' && localStorage.setItem('cpu', '');
+let cpu = localStorage.cpu;
+
+// Tile collections for cpu
 const buttonCollections = document.getElementById('gameCol').children;
 
+// Array to store current status of the grid
 let currentBoardStatus = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 let openPositions = currentBoardStatus;
 
+if (player1 === 'O' && cpu === 'X') {
+	handleCpuClick();
+}
+
 gameGrid.addEventListener('click', (e) => {
+	handleGame(e);
+});
+
+function handleGame(e) {
+	let turn = 0;
 	if (openPositions.length != 0) {
 		if (e.target.nodeName === 'BUTTON') {
-			handleUserClick(e, player1);
+			if (player2 != '' && cpu === '') {
+				turn === 0 ? handleUserClick(e, player1) : handleUserClick(e, player2);
+			} else {
+				handleUserClick(e, player1);
+			}
 			cpu != '' && handleCpuClick();
+
 			console.log('From click event: ', currentBoardStatus);
 		}
 	}
-	openPositions.length === 0 && console.log("It's a tie");
-	console.log('Open positions', openPositions);
-});
+	if (openPositions.length === 0) {
+		tiesNum += 1;
+		localStorage.ties = tiesNum.toString();
+		ties.innerHTML = tiesNum;
+	}
+}
 
 function handleUserClick(e, player) {
 	if (e.target.disabled != true) {
@@ -52,7 +80,7 @@ function handleUserClick(e, player) {
 			resultModal.classList.remove('hidden');
 			winNum += 1;
 			wins.innerHTML = winNum;
-			localStorage.setItem('wins', winNum.toString);
+			localStorage.setItem('wins', winNum.toString());
 		}
 		e.target.disabled = true;
 	}
@@ -60,24 +88,27 @@ function handleUserClick(e, player) {
 }
 
 function handleCpuClick() {
-	const randomTile =
-		openPositions[Math.floor(Math.random() * openPositions.length)];
+	if (openPositions.length != 0) {
+		const randomTile =
+			openPositions[Math.floor(Math.random() * openPositions.length)];
 
-	if (buttonCollections[randomTile].firstElementChild.disabled != true) {
-		buttonCollections[randomTile].firstElementChild.innerHTML =
-			cpu === 'X' ? xTileSVG : oTileSVG;
-		currentBoardStatus[buttonCollections[randomTile].firstElementChild.value] =
-			cpu;
+		if (buttonCollections[randomTile].firstElementChild.disabled != true) {
+			buttonCollections[randomTile].firstElementChild.innerHTML =
+				cpu === 'X' ? xTileSVG : oTileSVG;
+			currentBoardStatus[
+				buttonCollections[randomTile].firstElementChild.value
+			] = cpu;
 
-		if (checkWinner(currentBoardStatus, cpu)) {
-			result_card.innerHTML = `CPU wins`;
-			resultModal.classList.remove('hidden');
-			lossNum += 1;
-			wins.innerHTML = lossNum;
-			localStorage.setItem('loss', lossNum.toString);
+			if (checkWinner(currentBoardStatus, cpu)) {
+				result_card.innerHTML = `CPU wins`;
+				resultModal.classList.remove('hidden');
+				lossNum += 1;
+				wins.innerHTML = lossNum;
+				localStorage.setItem('loss', lossNum.toString());
+			}
+
+			buttonCollections[randomTile].firstElementChild.disabled = true;
 		}
-
-		buttonCollections[randomTile].firstElementChild.disabled = true;
 	}
 	openPositions = currentBoardStatus.filter((tile) => typeof tile === 'number');
 }
@@ -114,3 +145,9 @@ function checkWinner(currentBoardStatus, turn) {
 		return false;
 	}
 }
+
+// Button Logic
+
+// Reset Button
+// New Round Button
+// Quit Button
